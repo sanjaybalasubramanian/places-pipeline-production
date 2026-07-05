319598ecc241bde94ee041279c15dd60ddbad5b0
Sanjay <sanjayganeshb@gmail.com>
2026-07-06 01:40:36 +0530

    adding ssl variables

--- places_api_client.py
+++ places_api_client.py
@@ -1,3 +1,9 @@
+# [LOCAL UPDATE] Adding strict timeout and SSL verification
+import ssl
+timeout = 30
+ssl_verify = True
+
+
 # [COWORKER UPDATE v2.0]
 # Switched from Google Places to Mapbox API for cost reduction
 def fetch_mapbox_data():
